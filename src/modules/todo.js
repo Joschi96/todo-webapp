1dd38bc3472444168e5a450fb719093829e09b87
Joschi96 <117238956+Joschi96@users.noreply.github.com>
2024-10-04 15:02:14 +0200

    add Todo Module as class with setup for functions

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,29 +1,42 @@
 // Handles the creation and management of individual todo items
-// exports a factory function that creates a new todo item with properties title, description, dueDate, priority, and isComplete
 // Methods: functions to edit, toggle completion status, and mark as important
 
-export default function TodoItem(title, description, dueDate, priority, isComplete = false, list) {
+const todo = (() => {
+  class Todo {
+    constructor(title, description, dueDate, important, listId, todoId) {
+      this.title = title;
+      this.description = description;
+      this.dueDate = dueDate;
+      this.important = important;
+      this.listId = listId;
+      this.todoId = todoId;
+      this.isComplete = false;
+    }
+  }
+
+  function addTodo() {
+    // logic to add todo item
+  }
+
+  function editTodo() {
+    // logic to edit todo item
+  }
+
+  function deleteTodo() {
+    // logic to delete todo item
+  }
+
+  function toggleComplete() {
+    // logic to toggle completion status
+  }
+
+  function markImportant() {
+    // logic to mark as important
+  }
+
   return {
-    title,
-    description,
-    dueDate,
-    priority,
-    isComplete,
-    list,
-    toggleCompletion() {
-      this.isComplete = !this.isComplete;
-    },
-    toggleImportant() {
-      this.priority = !this.priority;
-    },
-    editTitle(newTitle) {
-      this.title = newTitle;
-    },
-    editDescription(newDescription) {
-      this.description = newDescription;
-    },
-    editDueDate(newDueDate) {
-      this.dueDate = newDueDate;
-    },
+
   };
-}
+})();
+
+export default todo;
