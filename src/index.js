222b6f8ddb5486e6cdbd7bb92568c4dfad8e8ba8
Joschi96 <117238956+Joschi96@users.noreply.github.com>
2024-10-06 20:48:33 +0200

    fix bug initial load did not work properly

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import lists from './modules/list.js';
 import staticHtml from './modules/staticHtml.js';
 import { loadData,saveData } from './modules/storage.js';
 import eventHandler from './modules/eventHandler.js';
+import activeTab from './modules/tabHandler.js';
 
 // Render static UI on page load
 //loadData('lists');
@@ -13,5 +14,7 @@ staticHtml.initializeHtml();
 saveData('lists',lists.listsArray);
 dom.showLists();
 
+
 eventHandler.addTabSwitchingEvent();
 eventHandler.addListCardEvent();
+activeTab.setActiveTab('all');
